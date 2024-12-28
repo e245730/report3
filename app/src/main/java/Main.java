eb539243709558c9198e92896eb24687ca8dd031
toguchisouho@toguchikanadefunonotobukkukonpyuta <e245730@ie.u-ryukyu.ac.jp>
2024-12-29 08:44:41 +0900

    report4 step1

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,13 +2,13 @@ import jp.ac.uryukyu.ie.e245730.*;
 //プッシュ練習2
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
-
-        System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
-
+        //Hero hero = new Hero("勇者", 10, 5);
+       // Enemy enemy = new Enemy("スライム", 6, 3);
+        LivingThing hero = new LivingThing("勇者１", 10, 5
+        );
+        LivingThing enemy = new LivingThing("スライム", 6, 3);
         int turn = 0;
-        while( hero.getDead() == false && enemy.getDead() == false ){
+        while( hero.isDead() == false && enemy.isDead() == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245730/LivingThing.java
@@ -0,0 +1,48 @@
+package jp.ac.uryukyu.ie.e245730;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+//コントラクタ
+    public LivingThing (String name, int hitPoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+    //getter
+    public String getName () {
+        return this.name;
+    }
+    public int getHitPoint () {
+        return this.hitPoint;
+    }
+    public int getAttack () {
+        return this.attack;
+
+    }
+    public boolean isDead () {
+        return this.dead;
+    }
+    
+    public void attack(LivingThing opponent){
+        if(!this.dead){
+        int damage = (int)(Math.random() * this.attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.name, opponent.getName(), damage);
+         opponent.wounded(damage);
+        }
+    }
+    
+    public void wounded(int damage){
+        this.hitPoint -= damage;
+        if( this.hitPoint < 0 ) {
+            this.dead = true;
+            System.out.printf("%sは倒れた。\n", this.name);
+        }
+    }
+    
+}
